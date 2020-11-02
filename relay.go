a535c6ac8a254c62906ff4771e4a0600d2fa5a36
Rusty Eddy <rusty@rustyeddy.com>
2020-11-02 09:14:40 -0800

    update the relay

--- relay.go
+++ relay.go
@@ -2,6 +2,7 @@ package station
 
 import (
 	"log"
+	"net/http"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"periph.io/x/periph/conn/gpio"
@@ -20,6 +21,8 @@ func (r *Relay) Set(l gpio.Level) {
 	r.Set(l)
 }
 
+// MessageHandler will accept messages from the mqtt Client to be processed
+// accordingly
 func (r *Relay) MessageHandler(c mqtt.Client, m mqtt.Message) {
 	t := m.Topic()
 	p := m.Payload()
@@ -30,4 +33,8 @@ func (r *Relay) MessageHandler(c mqtt.Client, m mqtt.Message) {
 		l = gpio.High
 	}
 	r.Set(l)
+}
+
+func (r *Relay) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
 }
